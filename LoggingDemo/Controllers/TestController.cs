607b16630992085fcd15028864041f740402ffc9
NilimaVadarevu <mangsule.nilima123@gmail.com>
2025-12-23 16:49:28 +0000

    Initial commit:LoogingDemo app

---
+++ LoggingDemo/Controllers/TestController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoggingDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        private readonly ILogger<TestController> _logger;
+        // Constructor dependency injection to provide an ILogger instance specific to the TestController.
+        // ASP.NET Core automatically injects the appropriate logger.
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation("LogInformation:TestController started");
+        }
+
+        [HttpGet("all-logs")]
+        public IActionResult LogAllLevels()
+        {
+            _logger.LogTrace("LogTrace:Entering the LogAllLevels endpoint with Trace-Level logging");
+
+            //Simulate a variable and log it at trace level
+            int cal = 5 * 10;
+            _logger.LogTrace($"LogTrace:Calculation :{cal}");
+
+            _logger.LogDebug("LogDebug:Initializing debug-level logs for debugging purpose");
+            var debugInfo = new { Action = "LogAllLevel", Status = "Debugging" };
+
+            _logger.LogDebug("LogDebug:Debug Information:{@debugInfo}",debugInfo);
+
+            _logger.LogInformation("LogInformation:The LogAllLevel endpoint was reached successfully");
+
+            //Simulate a condition that might cause an issue
+            bool isTakingMoreTime = true;
+            if(isTakingMoreTime)
+            {
+                _logger.LogWarning("LogWarning:External API taking more time to respond.Action may be required now");
+            }
+
+            try
+            {
+                //simulate error scenario
+                int x = 0;
+                int res = 10/x;
+
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, "LogError:An Error occurred");
+            }
+
+            //simulate a critical error scenario
+            bool criticalFailure = true;
+            if(criticalFailure)
+            {
+                _logger.LogCritical("LogCritical:A critical system failure has been detected:Immediate attention requires");
+            }
+
+            return Ok("All log levels demonstrated");
+        }
+
+    }
+}
---
+++ LoggingDemo/Program.cs
@@ -0,0 +1,49 @@
+
+namespace LoggingDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            ////configure Logging providers
+            //builder.Logging.ClearProviders();
+            //builder.Logging.AddConsole();
+            //builder.Logging.AddDebug();
+
+            ////set specific log levels for different libraries
+
+            //builder.Logging.SetMinimumLevel(LogLevel.Information);
+            //builder.Logging.AddFilter("Microsoft", LogLevel.Warning);
+            //builder.Logging.AddFilter("Microsoft.AspNetCore", LogLevel.Warning);
+            //builder.Logging.AddFilter("System", LogLevel.Warning);
+            //builder.Logging.AddFilter("LoggingDemo.Controllers", LogLevel.Trace);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
