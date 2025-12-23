0a699ec5e23cb7c3c72b147df74b11c4fb697fb4
NilimaVadarevu <mangsule.nilima123@gmail.com>
2025-12-23 17:06:44 +0000

    removed swagger in Program.cs

--- LoggingDemo/Program.cs
+++ LoggingDemo/Program.cs
@@ -30,11 +30,8 @@ namespace LoggingDemo
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+           
+           
 
             app.UseHttpsRedirection();
 
